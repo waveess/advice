25417467d6447a83022330d404f59351ad0a4025
rawanfatima <rawanfatima@outlook.com>
2021-11-27 19:05:29 -0800

    added the button functionality

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+    state = { advice: '' };
+
+    componentDidMount() {
+        // console.log('component did mount');
+       this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then((response) => {
+            const { advice } = response.data.slip;
+            this.setState({ advice });
+        })
+        .catch((error) => {
+    console.log(error);
+        });
+    }
+
+    render() {
+        const { advice } = this.state;
+        return(
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{advice}</h1>
+                    <button className = "button" onClick={this.fetchAdvice}>
+                        <span>Click For Advice</span>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
